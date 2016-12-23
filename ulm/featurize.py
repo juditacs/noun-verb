037c47414c31fb0f8b56f7dfaa95a84c087fec68
Judit Acs <judit@sch.bme.hu>
2016-12-23 21:57:42 +0100

    refactorized Featurizer

--- ulm/featurize.py
+++ ulm/featurize.py
@@ -6,241 +6,291 @@
 #
 # Distributed under terms of the MIT license.
 
-import logging
 
 from argparse import ArgumentParser
 from collections import defaultdict
+import re
 
-from sklearn.feature_extraction import DictVectorizer
-import numpy as np
+# from sklearn.feature_extraction import DictVectorizer
+# import numpy as np
 
 
-class InvalidTag(Exception):
+class InvalidInput(Exception):
     pass
 
 
-class GrepFilter:
-    def __init__(self, grep_filter, include_default=False):
-        self.grep_filter = grep_filter
-        if grep_filter is None:
-            self.all_pass = True
+class InvalidTag(InvalidInput):
+    pass
+
+
+class InvalidLine(InvalidInput):
+    pass
+
+
+class NotYetRefactored(Exception):
+    pass
+
+
+class Sample:
+
+    @classmethod
+    def from_line(cls, line):
+        try:
+            sample, label = line.split('\t')
+        except IndexError:
+            raise InvalidLine('Number of fields is not 2')
+        return cls(sample, label)
+
+    def __init__(self, sample, label):
+        self.sample = sample
+        self.label = label
+
+
+class DataSet:
+
+    def __init__(self, max_sample_per_class, skip_duplicates=True,
+                 max_limit=0, expected_class_no=2):
+        self.max_sample_per_class = max_sample_per_class
+        self._limit_per_class = max_sample_per_class > 0
+        self._skip_duplicates = skip_duplicates
+        self._max_limit = max_limit
+        self._full = False
+        self._sample_per_class_cnt = defaultdict(int)
+        self._expected_class_no = expected_class_no
+        if self._skip_duplicates:
+            self._samples = set()
         else:
-            self.all_pass = False
-            self.include_default = (len(self.grep_filter) == 1 or include_default)
+            self._samples = []
 
-    def match(self, label):
-        if self.all_pass is True:
-            return ""
-        for gf in self.grep_filter:
-            if gf in label:
-                return gf
-        if self.include_default:
-            return 'default'
+    def __len__(self):
+        return len(self._samples)
+
+    def get_sample_per_class(self, label):
+        return self._sample_per_class_cnt.get(label, 0)
+
+    @property
+    def full(self):
+        if self._full is False:
+            if self._max_limit > 0 and len(self._samples) > self._max_limit:
+                self._full = True
+            if self._limit_per_class and \
+                    len(self._sample_per_class_cnt) >= self._expected_class_no:
+                if all(cnt >= self.max_sample_per_class for cnt in
+                       self._sample_per_class_cnt.values()):
+                    self._full = True
+        return self._full
+
+    def add_sample(self, sample):
+        if self.full is True:
+            return False
+        if self._limit_per_class:
+            if self._sample_per_class_cnt[sample.label] >= \
+                    self.max_sample_per_class:
+                return False
+        self.__add_sample(sample)
+        return True
+
+    def __add_sample(self, sample):
+        if self._skip_duplicates:
+            self._samples.add(sample)
+        else:
+            self._samples.append(sample)
+        self._sample_per_class_cnt[sample.label] += 1
+
+
+class LabelExtractor:
+
+    def __init__(self, extractor=None):
+        self.extractor = extractor
+
+    def extract_label(self, label):
+        if self.extractor is None:
+            return label
+        return self.extractor(label)
+
+    def __call__(self, label):
+        return self.extractor(label)
+
+
+class WebCorpusExtractor:
+
+    def __init__(self, grep_filter=None, regex_filter=None):
+        self.echo = not grep_filter and not regex_filter
+        self.grep_filter = grep_filter
+        self.init_regex_filter(regex_filter)
+
+    def init_regex_filter(self, filters):
+        if filters is not None:
+            self.regex_filter = []
+            for rf in filters:
+                self.regex_filter.append(re.compile(rf, re.UNICODE))
+        else:
+            self.regex_filter = None
+
+    def __call__(self, tag):
+        return self.extract_label(tag)
+
+    def extract_label(self, field):
+        if self.echo:
+            return field
+        if self.regex_filter is None:
+            return self.__apply_grep_filter(field)
+        if self.grep_filter is not None:
+            for gf in self.grep_filter:
+                if gf in field:
+                    rf = self.__match_regex_filter(field)
+                    if rf is not None:
+                        return '{}{}'.format(gf, rf)
+        else:
+            return self.__match_regex_filter(field)
         return None
-        raise RuntimeError("Label should have been skipped earlier: {}".format(
-            label))
 
-    def skip(self, label):
-        # FIXME
-        if label is None:
-            return False
-        if self.all_pass is True or self.include_default is True:
-            return False
-        if all(gf not in label for gf in self.grep_filter) and \
-                self.include_default is False:
-            return True
-        return False
+    def __match_regex_filter(self, field):
+        for r in self.regex_filter:
+            m = r.search(field)
+            if m:
+                rf = m.group(1)
+                return rf
+        return None
+
+    def __apply_grep_filter(self, field):
+        if self.grep_filter:
+            for gf in self.grep_filter:
+                if gf in field:
+                    return gf
+        return None
 
 
 class Featurizer:
     def __init__(self, max_sample_per_class,
                  max_lines=0, skip_duplicates=True,
-                 label_filter=None, grep_filter=None):
-        self.max_sample_per_class = max_sample_per_class
-        self.maximize_sample_per_class = max_sample_per_class > 0
-        self.max_lines = max_lines
-        self.line_cnt = 0
-        self.skip_duplicates = skip_duplicates
-        self.uniq_samples = set()
-        self.samples = []
-        self.labels = []
-        self.sample_per_class = defaultdict(int)
-        self.label_filter = None if label_filter is None else \
-            set(label_filter)
-        self.grep_filter = GrepFilter(grep_filter)
-        self.raw_samples = []
+                 label_extractor=None):
+        self.dataset = DataSet(
+            max_sample_per_class=max_sample_per_class,
+            skip_duplicates=skip_duplicates,
+        )
+        self.label_extractor = LabelExtractor(label_extractor)
+        self._max_lines = max_lines
+        self._line_cnt = 0
 
-    def featurize_file(self, fn):
-        with open(fn) as f:
-            for line in f:
-                self.line_cnt += 1
-                if self.continue_reading() is False:
-                    break
-                if self.skip_line(line):
-                    continue
-                try:
-                    sample = self.extract_sample_from_line(line)
-                except InvalidTag:
-                    continue
-                if self.skip_sample(sample):
-                    continue
-                self.featurize_and_store_sample(sample)
-                self.update_counters(sample)
-
-    def update_counters(self, sample):
-        x, y = sample
-        self.sample_per_class[y] += 1
-        if self.skip_duplicates:
-            self.uniq_samples.add((x, y))
+    def featurize_file(self, stream):
+        for line in stream:
+            if self.continue_reading() is False:
+                break
+            try:
+                sample = self.extract_sample_from_line(line)
+            except InvalidInput:
+                continue
+            self.dataset.add_sample(sample)
+            self.featurize_sample(sample)
 
     def continue_reading(self):
-        if self.max_lines > 0 and self.line_cnt >= self.max_lines:
+        self._line_cnt += 1
+        if self._max_lines > 0 and self._line_cnt > self._max_lines:
             return False
-        if self.maximize_sample_per_class and len(self.sample_per_class) > 1:
-            if all(v >= self.max_sample_per_class
-                   for v in self.sample_per_class.values()):
-                return False
-        return True
-
-    def skip_line(self, line):
-        if self.skip_duplicates and line in self.uniq_samples:
-            return True
-        if not line.strip():
-            return True
-        if 'UNKNOWN' in line or '??' in line:
-            return True
-        return False
-
-    def skip_sample(self, sample):
-        word, (pos, tag) = sample
-        label = (pos, tag)
-        if self.grep_filter.skip(tag):
-            return True
-        if self.label_filter is not None and pos not in self.label_filter:
-            return True
-        if self.maximize_sample_per_class and \
-                self.sample_per_class[label] >= self.max_sample_per_class:
-            return True
-        return False
+        return not self.dataset.full
 
     def extract_sample_from_line(self, line):
+        if not line.strip() or 'UNKNOWN' in line or '??' in line:
+            raise InvalidLine()
         fd = line.strip().split('\t')
         if len(fd) < 2:
-            raise InvalidTag("Not enough fields.")
+            raise InvalidLine("Not enough fields.")
         word, tag = fd[:2]
-        try:
-            pos = tag.split('/')[1].split('<')[0].split('[')[0]
-        except IndexError:
-            raise InvalidTag("POS cannot be extracted from")
-        if self.label_filter is None:
-            pos = ""
-        if self.grep_filter:
-            return word, (pos, self.grep_filter.match(tag))
-        return word, (pos, pos)
+        label = self.label_extractor(tag)
+        if label is None:
+            raise InvalidTag()
+        return Sample(word, label)
 
-    def featurize_and_store_sample(self, sample):
-        label = '{}_{}'.format(sample[1][0], sample[1][1])
-        word = sample[0]
-        x, y = self.featurize((word, label))
-        self.raw_samples.append((x, y))
-        self.samples.append(x)
-        self.labels.append(y)
-
-    def featurize(self, sample):
-        raise NotImplementedError("Feature extraction should be"
-                                  "implemented in inheriting classes")
-
-    def create_matrices(self):
-        self.X = self.vectorize('x_vectorizer', self.samples)
-        self.y = self.vectorize('y_vectorizer', self.labels)
-        return self.X, self.y
-
-    def vectorize(self, name, data):
-        if not hasattr(self, name):
-            setattr(self, name, DictVectorizer())
-            return getattr(self, name).fit_transform(data)
-        return getattr(self, name).transform(data)
+    def featurize_sample(self, sample):
+        sample.features = {'word': sample.sample}
 
 
-class CharacterSequenceFeaturizer(Featurizer):
+class NGramFeaturizer:
+    def __init__(self, N, last_char, max_sample_per_class,
+                 max_lines=0, skip_duplicates=True,
+                 label_extractor=None, bag_of=False,
+                 use_padding=True):
+        super().__init__(max_sample_per_class, max_lines=max_lines,
+                         skip_duplicates=skip_duplicates,
+                         label_extractor=label_extractor)
+        self.N = N
+        self.last_char = last_char
+        self.bag_of = bag_of
+        self.use_padding = use_padding
+
+    def featurize_sample(self, sample):
+        text = sample.sample
+        text = text[-self.last_char:]
+        if self.use_padding:
+            text = '{0}{1}{0}'.format(' ' * (self.N-1), text)
+        if self.bag_of is True:
+            sample.features = NGramFeaturizer.extract_bagof_ngrams(
+                text, self.N)
+        else:
+            sample.features = NGramFeaturizer.extract_positional_ngrams(
+                text, self.N)
+
+    @staticmethod
+    def extract_bagof_ngrams(text, N):
+        d = {}
+        for i in range(0, len(text)-N):
+            d[text[i:i+N]] = True
+        return d
+
+    @staticmethod
+    def extract_positional_ngrams(text, N):
+        d = {}
+        for i in range(0, len(text)-N):
+            d[i] = text[i:i+N]
+        return d
+
+
+class CharacterSequenceFeaturizer:
 
     hu_accents = 'áéíóöőúüű'
     hu_chars = 'abcdefghijklmnopqrstuvwxyz' + hu_accents
     punct = '-_.'
     all_chars = hu_chars + punct + '*0'
 
-    def __init__(self, max_sample_per_class, max_len,
-                 tolower=True, replace_rare=True,
-                 max_lines=0, skip_duplicates=True, **kwargs):
-        super().__init__(max_sample_per_class, max_lines, skip_duplicates,
-                         **kwargs)
-        self.tolower = tolower
+    def __init__(self, max_len, max_sample_per_class, tolower=True,
+                 replace_rare=True,
+                 max_lines=0, skip_duplicates=True,
+                 label_extractor=None, bag_of=False,
+                 use_padding=True):
+        super().__init__(max_sample_per_class, max_lines=max_lines,
+                         skip_duplicates=skip_duplicates,
+                         label_extractor=label_extractor)
         self.max_len = max_len
+        self.create_char_mapping()
+        self.tolower = tolower
         self.replace_rare = replace_rare
         self.classes = set()
-        self.create_char_mapping()
-        #self.create_x_vectorizer()
-
-    def create_char_mapping(self):
-        self.char_mapping = {
-            c: i for i, c in enumerate(CharacterSequenceFeaturizer.all_chars)
-        }
-
-    def create_x_vectorizer(self):
-        l = []
-        for c in CharacterSequenceFeaturizer.all_chars:
-            l.append({'char': c})
-        self.x_vectorizer = DictVectorizer()
-        self.x_vectorizer.fit_transform(l)
-
-    def featurize(self, sample):
-        text = self.normalize(sample[0])
-        tag = sample[1]
-        self.classes.add(tag)
-        feats = text
-        return (feats, {'label': tag})
-
-    def normalize(self, text):
-        if self.tolower:
-            text = text.lower()
-        out = []
-        for c in text:
-            if c.isdigit():
-                out.append('0')
-            elif c in CharacterSequenceFeaturizer.all_chars:
-                out.append(c)
-            else:
-                out.append('*')
-        return ''.join(out)
-
-    def create_matrices(self):
-        X = np.zeros((len(self.samples), self.max_len, len(self.char_mapping)),
-                     dtype=np.bool)
-        for i, s in enumerate(self.samples):
-            stop = -self.max_len if len(s) >= self.max_len else -len(s)
-            for j in range(-1, stop, -1):
-                X[i, j, self.char_mapping[s[j]]] = 1
-        self.X = X
-        self.y = self.vectorize('y_vectorizer', self.labels)
-        return self.X, self.y.toarray()
-
-    @property
-    def data_dim(self):
-        return (len(self.samples), self.max_len,
-                len(self.char_mapping), len(self.classes))
+        # TODO
 
 
 def parse_args():
     p = ArgumentParser()
-    p.add_argument('-i', type=str)
+    p.add_argument('-i', type=str,
+                   default='/mnt/store/hlt/Language/Hungarian/' +
+                   'Corp/webcorp_analed/xaa.tagged.utf8')
+    p.add_argument('-s', '--sample-per-class', type=int, default=5)
+    p.add_argument('-f', '--filter', type=str, default='NOUN,VERB')
     return p.parse_args()
 
 
 def main():
     args = parse_args()
-    c = CharacterSequenceFeaturizer(1, 3)
-    c.featurize_file(args.i)
+    filt = args.filter.split(',')
+    s = args.sample_per_class
+    wc = WebCorpusExtractor(grep_filter=filt)
+    f = Featurizer(max_sample_per_class=s, max_lines=20, label_extractor=wc)
+    with open(args.i) as f:
+        f.featurize_file(f)
+    for s in f.dataset._samples:
+        print(s.sample, s.label)
 
 
 if __name__ == '__main__':
     main()
+# import logging
