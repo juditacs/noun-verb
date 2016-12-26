80486ed395a11d9d62ffaa77f51be594198ded57
Judit Acs <judit@sch.bme.hu>
2016-12-27 00:53:40 +0100

    a few more tests

--- ulm/tests.py
+++ ulm/tests.py
@@ -15,8 +15,7 @@ terítse	terít/VERB
 pillanat	pillanat/NOUN
 mozgásban	mozgás/NOUN
 mi	mi/NOUN
-mindenre	minden/NOUN
-"""
+mindenre	minden/NOUN"""
 
 input_with_cases = u"""mereven	merev/ADJ[MANNER]/ADV
 A	a/ART
@@ -29,8 +28,14 @@ mi	mi/NOUN<PERS<1>><PLUR>
 mindenre	minden/NOUN<CAS<SBL>>
 önmagában	önmaga/NOUN<PERS><CAS<INE>>
 széksorokban	széksor/NOUN<PLUR><CAS<INE>>
-háttérre	háttér/NOUN<CAS<SBL>>
-"""
+háttérre	háttér/NOUN<CAS<SBL>>"""
+
+input_with_duplicates = u"""s1	l1
+s1	l2
+s2	l1
+s1	l1
+s1	l2
+s2	l2"""
 
 
 class FeaturizerTest(unittest.TestCase):
@@ -54,6 +59,13 @@ class FeaturizerTest(unittest.TestCase):
         f = featurize.Featurizer(2, 20, label_extractor=w)
         f.featurize_file(io.StringIO(input_with_cases))
         self.assertEqual(len(f.dataset), 4)
+
+    def test_regex_extract2(self):
+        w = featurize.WebCorpusExtractor(regex_filter=[
+            r'<CAS<([^<>]+)>',
+        ])
+        f = featurize.Featurizer(2, 20, label_extractor=w)
+        f.featurize_file(io.StringIO(input_with_cases))
         self.assertTrue(f.dataset.full)
 
     def test_regex_extract_not_enough_input(self):
@@ -63,16 +75,37 @@ class FeaturizerTest(unittest.TestCase):
         f = featurize.Featurizer(6, 11, label_extractor=w)
         f.featurize_file(io.StringIO(input_with_cases))
         self.assertEqual(len(f.dataset), 4)
-        self.assertFalse(f.dataset.full)
 
     def test_regex_extract_not_enough_input2(self):
         w = featurize.WebCorpusExtractor(regex_filter=[
             r'<CAS<([^<>]+)>',
         ])
-        f = featurize.Featurizer(3, 15, label_extractor=w)
+        f = featurize.Featurizer(6, 11, label_extractor=w)
         f.featurize_file(io.StringIO(input_with_cases))
-        self.assertEqual(len(f.dataset), 5)
         self.assertFalse(f.dataset.full)
+
+    def test_empty_extractor(self):
+        s = len(input_with_cases.strip().split('\n'))
+        f = featurize.Featurizer(3)
+        f.featurize_file(io.StringIO(input_with_cases))
+        self.assertEqual(len(f.dataset), s)
+
+    def test_empty_extractor2(self):
+        f = featurize.Featurizer(3)
+        f.featurize_file(io.StringIO(input_with_cases))
+        self.assertIn('részletez/VERB<INF>', f.dataset.labels)
+
+    def test_keep_duplicates(self):
+        s = len(input_with_duplicates.split('\n'))
+        f = featurize.Featurizer(30, 300, skip_duplicates=False)
+        f.featurize_file(io.StringIO(input_with_duplicates))
+        self.assertEqual(len(f.dataset), s)
+
+    def test_skip_duplicates(self):
+        s = len(set(input_with_duplicates.split('\n')))
+        f = featurize.Featurizer(30, 300, skip_duplicates=True)
+        f.featurize_file(io.StringIO(input_with_duplicates))
+        self.assertEqual(len(f.dataset), s)
 
 
 class WebCorpusExtractorTest(unittest.TestCase):
@@ -102,6 +135,61 @@ class WebCorpusExtractorTest(unittest.TestCase):
         )
         self.assertEqual(w.extract_label("NOUN<CAS<ACC>"), "NOUNACC")
         self.assertEqual(w.extract_label("<CAS<ACC>"), None)
+
+
+class NGramFeaturizerTest(unittest.TestCase):
+
+    def test_padding_positional(self):
+        f = featurize.NGramFeaturizer(2, 3,
+                                      max_sample_per_class=2, use_padding=True)
+        f.featurize_file(io.StringIO("abc\tdef"))
+        features = list(f.get_samples())[0].features
+        self.assertEqual(features, {0: ' a', 1: 'ab', 2: 'bc', 3: 'c '})
+
+    def test_no_padding_positional(self):
+        f = featurize.NGramFeaturizer(2, 3, max_sample_per_class=2,
+                                      use_padding=False)
+        f.featurize_file(io.StringIO("abc\tdef"))
+        features = list(f.get_samples())[0].features
+        self.assertEqual(features, {0: 'ab', 1: 'bc'})
+
+    def test_padding_bagof(self):
+        f = featurize.NGramFeaturizer(2, 5, max_sample_per_class=2,
+                                      use_padding=True, bagof=True)
+        f.featurize_file(io.StringIO("abcab\tdef"))
+        features = list(f.get_samples())[0].features
+        self.assertEqual(features,
+                         {'ab': True, 'bc': True, 'ca': True,
+                          ' a': True, 'b ': True})
+
+    def test_no_padding_bagof(self):
+        f = featurize.NGramFeaturizer(2, 5, max_sample_per_class=2,
+                                      use_padding=False, bagof=True)
+        f.featurize_file(io.StringIO("abcab\tdef"))
+        features = list(f.get_samples())[0].features
+        self.assertEqual(features, {'ab': True, 'bc': True, 'ca': True})
+
+    def test_last_char(self):
+        f = featurize.NGramFeaturizer(2, 3, max_sample_per_class=2,
+                                      use_padding=False, bagof=False)
+        f.featurize_file(io.StringIO("abcdef\tdef"))
+        features = list(f.get_samples())[0].features
+        self.assertEqual(features, {0: 'de', 1: 'ef'})
+
+    def test_last_char_with_padding(self):
+        f = featurize.NGramFeaturizer(2, 3, max_sample_per_class=2,
+                                      use_padding=True, bagof=False)
+        f.featurize_file(io.StringIO("abcdef\tdef"))
+        features = list(f.get_samples())[0].features
+        self.assertEqual(features, {0: ' d', 1: 'de', 2: 'ef', 3: 'f '})
+
+
+class CharacterSequenceFeaturizerTester(unittest.TestCase):
+
+    def test_init(self):
+        f = featurize.CharacterSequenceFeaturizer(5, 10)
+        self.assertIsInstance(f, featurize.Featurizer)
+        self.assertEqual(f.dataset.max_sample_per_class, 10)
 
 if __name__ == '__main__':
     unittest.main()
